d9353a84be8888c8b1a732e4ad9b988d55a5dc7a
ArturSuperpizza <129962627+ArturSuperpizza@users.noreply.github.com>
2023-04-07 09:53:21 +0100

    Add files via upload

---
+++ Birthday.js
@@ -0,0 +1,7 @@
+console.log("Happy Birthday"); String
+
+function button(){
+    x = document.getElementById("button")
+    x.innerHTML = "Click me for a SURPRISE!!!"
+    x.innerHTML = "Happy Birthday!!"
+}
